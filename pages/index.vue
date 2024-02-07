ee34c83cdbc520d79e580778cdbfa07cd80d75fd
timmy <timmy0x@proton.me>
2024-02-07 15:52:02 -0500

    added timmygrad

--- pages/index.vue
+++ pages/index.vue
@@ -12,15 +12,12 @@
           <a href="https://twitter.com/gpucowboy">Twitter</a>
         </p>
         <p>
-          I study <strong>computer engineering</strong> & research <strong>compilers</strong> under
-          <a href="https://engineering.purdue.edu/ECE/News/2023/prof-milind-kulkarni-takes-over-as-interim-head-of-elmore-family-school-of-electrical-&-computer-engineering" target="_blank">Professor Kulkarni</a>
-          at Purdue University.
-          I'm fascinated by the <strong>relationships between computer hardware & software</strong>
-          & spend my weekends using them to accelerate <strong>machine learning</strong>. 
+          I study <strong>computer architecture</strong> & <strong>compilers</strong> at Purdue University.
+          I'm fascinated by computer design and research hardware/software systems for machine learning.
         </p><p>
-          I've been accepted to graduate school at Purdue & am <strong>looking for an internship</strong> at the <strong>intersection</strong> of <strong>machine learning & computer systems</strong>.
+          I've been accepted to graduate school and am <strong>looking for internships</strong> in AI & computer systems</strong>.
           If any of the below is interesting feel free to grab my 
-          <strong><a href="https://docs.google.com/document/d/1NOS2OGzGnHa1Qe5jc1MfnIMcf8BYE1qIFokDsozPFa0/edit?usp=sharing" target="_blank"><u>resume</u></a></strong> & <strong>lets chat!</strong>
+          <strong><a href="https://docs.google.com/document/d/1NOS2OGzGnHa1Qe5jc1MfnIMcf8BYE1qIFokDsozPFa0/edit?usp=sharing" target="_blank"><u>resume</u></a></strong> & <strong>let's chat!</strong>
         </p>
         <p>
           Below is a quick roster of my work :)
@@ -36,66 +33,56 @@
         <h2>Projects</h2>
         <h3><a href="https://github.com/iu-parfunc/gibbon" target="_blank">Gibbon Compiler</a></h3>
         <p>
-          A haskell compiler for operating on serial data. 
-          Gibbon transforms pointer based data representations into packed representations, & as a result yields much faster traversals for structures like binary trees.
+          A Haskell compiler for operating on serial data. 
+          Gibbon transforms pointer-based data representations into packed representations. As a result, Gibbon yields much faster traversals for structures like trees.
         </p>
 
         <h3><a href="https://engineering.purdue.edu/SoC-Team" target="_blank">AFTx07 - Digital Design & Tapeout</a></h3>
         <p>
-          A RISC-V microcontroller designed from scratch by the Purdue SoCET team.
-          Currently I am working on the tapeout flow to take our digital designs to manufacture, specifcally place 'n route & post-layout verification.
-          For digital design I worked on pipelining cache <-> memory transactions.
+          A RISC-V microcontroller designed from scratch by the Purdue SoCET team. 
+          I took part in digital design and now I'm working on the tapeout effort.
+        </p>
+
+        <h3><a href="https://github.com/0xtimmy/timmygrad.cu">Timmygrad</a></h3>
+        <p>
+          A simple autograd and backpropagation engine built in CUDA C. 
+          Designed to expose how memory is handled and gradients are computed during training.
+          Implements transformers and comes with the tiny_shakespeare dataset as a demo.
         </p>
 
         <h3>Glados</h3>
         <p>
-          A AI voice assistant built from scratch on an NVIDIA Jetson. 
-          Designed as an experiment in lifelong learning, it sports an open source LLM, vector database of textbooks, academic litterature & past conversation.
-          Still in progress...
+          An AI voice assistant built from scratch on an NVIDIA Jetson. 
+          Designed as an experiment in lifelong learning, it sports an open-source LLM and vector database of textbooks, academic literature  & past conversations. <br>
+          eternally in progress...
         </p>
 
-        <h3>MIPS Multicore</h3>
+        <h3>MIPS Multicore Processor</h3>
         <p>
-          A fully functional multicore, MIPS processor. Built from scratch for ECE 437. Can't link becasue prof will fail me :(
+          A fully functional multicore, MIPS processor. Built from scratch. I can't link the repo because of academic integrity :( but if you go to Purdue I have office hours on Saturdays.
         </p>
 
         <h3><a href="https://github.com/0xtimmy/web-diffusion" target="_blank">Web Diffusion</a></h3>
         <p>
-          A bare bones image <a href="https://arxiv.org/pdf/2006.11239.pdf" target="_blank">diffuser</a> that runs in the browser. 
-          It uses a <a href="https://arxiv.org/pdf/1706.03762.pdf" target="_blank">transformer</a> based model & custom GPU kernels that are run through <a href="https://developer.chrome.com/blog/webgpu-release/" target="_blank">WebGPU</a>.
+          A bare-bones image <a href="https://arxiv.org/pdf/2006.11239.pdf" target="_blank">diffuser</a> that runs in the browser. 
+          It uses a <a href="https://arxiv.org/pdf/1706.03762.pdf" target="_blank">transformers</a>-CNN hybrid model & custom GPU kernels that are run through <a href="https://developer.chrome.com/blog/webgpu-release/" target="_blank">WebGPU</a>.
           You can access it live <a href="https://web-diffusion.pages.dev/" target="_blank">here</a>. 
         </p>
 
         <h3><a id="engram" href="https://github.com/holium/engram" target="_blank">Engram</a></h3>
         <p>
-          An infrastructure-less collaborative document editor for the <a href="https://urbit.org/" target="_blank">urbit</a> ecosystem. Built with prosemirror, yjs & our own custom crdts. <br>
+          An infrastructure-less collaborative document editor for the <a href="https://urbit.org/" target="_blank">Urbit</a> ecosystem. Built with Prosemirror, Yjs & our own custom CRDTs. <br>
           <a href="https://www.youtube.com/watch?t=570&v=Q-MtMu-Jbf4&feature=youtu.be" target="_blank">At Assembly</a>
           <a href="https://twitter.com/jmrphy/status/1604583510014058497" target="_blank">On Twitter</a>
         </p>
 
         <h3><a href="https://github.com/0xtimmy/Ray-Tracing" target="_blank">Ray Tracing in One Week</a></h3>
         <p>
-          A c++ script that uses ray tracing to render a scene. Inspired by the <a href="https://raytracing.github.io/books/RayTracingInOneWeekend.html">Ray Tracing in One Weekend</a> book series.
+          A C++ script that uses ray tracing to render a scene. Inspired by the <a href="https://raytracing.github.io/books/RayTracingInOneWeekend.html">Ray Tracing in One Weekend</a> book series.
         </p>
         <h3><a href="https://0xtimmy.github.io/wiz-animations/" target="_blank">Wizards Animator</a></h3>
         <p>
           A script to give your <a href="https://wizardsdao.com/" target="_blank">wizard's dao</a> NFT fun lil animations.
-        </p>
-      </section>
-      <section>
-        <h2>Personal History</h2>
-        <p>
-          I started <strong>researching distributed systems</strong> in my dorm room & after my first year of college <strong>I left to turn my work into a company</strong>.
-          Our focus was <strong>improving performance via network locality</strong>: if we can avoid using a server, clients only have to communicate with each other. 
-          Done right this is much faster.
-          At some point I started playing with image generation models & 
-          I realized <strong>machine learning</strong> was a much more <strong>important</strong> & <strong>daughting</strong> performance challenge than what we had been working on.
-          <br><br>
-          So I <strong>returned to Purdue</strong> to study the full breadth of computers, <strong>from high level software down to the silicon</strong>, in search for a solution.
-          Most notably I joined the compilers research team & the SoC research team, to get comfortable working on the <strong>hardware/software boundary</strong> from both sides.
-          <br><br>
-          AI is going to demand a new class of computer, like the iPhone for mobile in 2007, that can meet the specific characteristics of ML operations.
-          <strong>It is the purpose of my studies & work to engineer this next generation of computers.</strong>
         </p>
       </section>
     </main>
